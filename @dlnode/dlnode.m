a32752e79dbf2f8e7ed0c828948a61fb1d379b44
Bhaskar <Bhaskar.techiepro@gmail.com>
2022-02-23 07:43:07 +0530

    dlnode completed

--- @dlnode/dlnode.m
+++ @dlnode/dlnode.m
@@ -12,5 +12,59 @@ classdef dlnode < handle
                 node.Data = Data;
             end
         end
+        function insertAfter(newN, nBefore)
+            removeNode(newN);
+            newN.Next = nBefore.Next;
+            newN.Prev = nBefore;
+            if ~isempty(newN.Next)
+                nBefore.Next.Prev = newN;
+            end
+            nBefore.Next = newN;
+        end
+        function insertBefore(newN, nAfter)
+            removeNode(newN);
+            newN.Next = nAfter;
+            newN.Prev = nAfter.Prev;
+            if ~isempty(newN.Prev)
+                nAfter.Prev.Next = newN;
+            end
+            nAfter.Prev = newN;
+        end
+        function removeNode(n)
+            if ~isscalar(n)
+                error('Nodes must be scalar')
+            end
+            pNode = n.Prev;
+            nNode = n.Next;
+            if ~isempty(pNode)
+                pNode.Next =  nNode;
+            end
+            if ~isempty(nNode)
+                pNode.Prev = pNode;
+            end
+            n.Next = dlnode.empty;
+            n.Prev = dlnode.empty;
+        end
+        function clearList(n)
+            prev = n.Prev;
+            next = n.Next;
+            removeNode(n)
+            while ~isempty(next)
+                n = next;
+                next = n.Next;
+                removeNode(n)
+            end
+            while ~isempty(prev)
+                n = prev;
+                prev = n.Prev;
+                removeNode(n)
+            end
+        end
     end
+    methods(Access = private)
+        function delete(node)
+            clearList(node);
+        end
+    end
+    
 end
